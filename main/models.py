e950b1b0dd2c219bd9fcaf212448cf0b5174e443
Ishraq Khan <khan136@purdue.edu>
2018-04-14 01:10:14 -0700

    Retrieved API key from TMDB API. Created Movies module

--- main/models.py
+++ main/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
+import Broadway.settings as settings
+import tmdb3
 # Create your models here.
+
+class Movie(models.Model):
+    #movie has id in database
+    id = models.IntegerField()
+    #title of movie
+    title = models.CharField(max_length=100)
+    #overview of movie
+    overview = models.TextField()
+    #movie release date
+    release_date = models.DateField()
+    #user ratings of movie
+    rating = models.FloatField()
+    #poster representing movie
+    poster = models.ImageField()
+    #genre
+    genre = models.CharField(max_length=100)
+
+    #return movie title as object descriptor
+    def __str__(self):
+        return self.title
+
+
+
+
+
+

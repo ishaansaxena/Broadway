7b6fb6b05e9f9dd19ee4d0894a975d8ba78b4e24
Ishaan <isaxena@purdue.edu>
2018-04-21 20:18:13 -0400

    Change movie model a lil bit

--- main/models.py
+++ main/models.py
@@ -6,26 +6,16 @@ import Broadway.settings as settings
 # Create your models here.
 
 class Movie(models.Model):
-    #id for db
-    id = models.IntegerField(primary_key=True)
     #movie has id in database
     movie_id = models.IntegerField(null=True)
     #title of movie
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=100, null=True, blank=True)
     #overview of movie
-    overview = models.TextField()
+    overview = models.TextField(null=True, blank=True)
     #movie release date
-    release_date = models.DateField()
-    #user ratings of movie
-    rating = models.FloatField()
+    release_date = models.DateField(null=True, blank=True)
     #poster representing movie
-    poster = models.ImageField(    
-        upload_to='static/assets/movies_images/',
-        default='static/assets/movies_images/default.png',
-        blank=True
-    )
-    #genre
-    genre = models.CharField(max_length=100)
+    poster = models.SlugField(null=True, blank=True)
 
     #return movie title as object descriptor
     def __str__(self):

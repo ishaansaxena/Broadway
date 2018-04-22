ce3587a76b7eef06782bda86a8681fca36ea7675
Swaraj Bhaduri <sbhadur@purdue.edu>
2018-04-21 21:05:47 -0400

    Get movies

--- main/urls.py
+++ main/urls.py
@@ -5,5 +5,6 @@ from . import views
 # Index for the Broadway App
 urlpatterns = [
     path('', views.index, name='index'),
-    url(r'^q/(?P<query>[\w\-&]+)/$', views.search, name='search')
+    url(r'^q/(?P<query>[\w\-&]+)/$', views.search, name='search'),
+    url(r'^m/(?P<ids>[0-9]+)/$', views.moviedetails, name='moviedetails')
 ]

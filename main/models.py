79ab5e917a70dee4dfe7c9f31e733b98baa73487
Ishaan <isaxena@purdue.edu>
2018-04-21 20:43:06 -0400

    swaraj pls

--- main/models.py
+++ main/models.py
@@ -17,22 +17,11 @@ class Movie(models.Model):
     #user ratings of movie
     rating = models.FloatField(null=True)
     #poster representing movie
-    poster = models.ImageField(    
+    poster = models.ImageField(
         upload_to='static/assets/movies_images/',
         default='static/assets/movies_images/default.png',
         blank=True
     )
-
-    poster = models.ImageField(null=True)
-    #genre
-    genre = models.CharField(max_length=100, null=True)
-    title = models.CharField(max_length=100, null=True, blank=True)
-    #overview of movie
-    overview = models.TextField(null=True, blank=True)
-    #movie release date
-    release_date = models.DateField(null=True, blank=True)
-    #poster representing movie
-    poster = models.SlugField(null=True, blank=True)
 
     #return movie title as object descriptor
     def __str__(self):
